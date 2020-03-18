7926b4c29fb7730bc3841e25fc281d64f7061564
eoinahern <eoin.ahern@mycit.ie>
2020-03-18 14:36:21 +0000

    add src folder

---
+++ src/api/domain/github/create_repo.go
@@ -0,0 +1,15 @@
+package github
+
+type CreateRepoRequest struct {
+	Name        string
+	Description string
+	Homepage    string
+	Private     bool
+	HasIssues   bool
+	HasProjects bool
+	HasWiki     bool
+}
+
+func CreateRepo() {
+
+}
